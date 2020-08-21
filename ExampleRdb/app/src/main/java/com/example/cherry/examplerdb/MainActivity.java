79975d24e94455cdf029baa3edb57943fda3c2f4
chaitanya <chaitanyakarri963@gmail.com>
2020-08-21 17:08:27 +0530

    21-08-2020

--- ExampleRdb/app/src/main/java/com/example/cherry/examplerdb/MainActivity.java
+++ ExampleRdb/app/src/main/java/com/example/cherry/examplerdb/MainActivity.java
@@ -1,15 +1,25 @@
 package com.example.cherry.examplerdb;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.cherry.examplerdb.Database.RTable;
+import com.example.cherry.examplerdb.Database.RViewModel;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     EditText name,roll,number;
     RecyclerView rv;
+    public static RViewModel viewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,8 +28,29 @@ public class MainActivity extends AppCompatActivity {
         roll = findViewById(R.id.roll);
         number = findViewById(R.id.number);
         rv = findViewById(R.id.rv);
+        viewModel = new ViewModelProvider(this).get(RViewModel.class);
+        /*To read the data from the room database*/
+        viewModel.readData().observe(this, new Observer<List<RTable>>() {
+            @Override
+            public void onChanged(List<RTable> rTables) {
+                MyAdapter adapter = new MyAdapter(MainActivity.this,rTables);
+                rv.setAdapter(adapter);
+                rv.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+            }
+        });
     }
 
     public void save(View view) {
+        String sname=name.getText().toString();
+        String sroll = roll.getText().toString();
+        String snum = number.getText().toString();
+        /*To insert the Data*/
+        RTable rTable = new RTable();
+        rTable.setName(sname);
+        rTable.setRoll(sroll);
+        rTable.setNumber(snum);
+        viewModel.insert(rTable);
+        Toast.makeText(this, "Data Inserted", Toast.LENGTH_SHORT).show();
+
     }
 }
--- ExampleRdb/app/src/main/java/com/example/cherry/examplerdb/MyAdapter.java
+++ ExampleRdb/app/src/main/java/com/example/cherry/examplerdb/MyAdapter.java
@@ -4,14 +4,18 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.cherry.examplerdb.Database.RTable;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 import java.util.List;
 
@@ -30,10 +34,18 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.HoldView> {
     }
 
     @Override
-    public void onBindViewHolder(@NonNull HoldView holder, int position) {
+    public void onBindViewHolder(@NonNull HoldView holder, final int position) {
         holder.name.setText(list.get(position).getName());
         holder.roll.setText(list.get(position).getRoll());
         holder.number.setText(list.get(position).getNumber());
+        holder.del.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MainActivity.viewModel.delete(list.get(position));
+                Toast.makeText(context, "Item Deleted",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
@@ -51,6 +63,50 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.HoldView> {
             name = itemView.findViewById(R.id.name);
             roll = itemView.findViewById(R.id.roll);
             number  = itemView.findViewById(R.id.number);
+            edit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    ViewGroup viewGroup = view.findViewById(R.id.content);
+                    View v = LayoutInflater.from(context).
+                            inflate(R.layout.update,viewGroup,false);
+
+                    final EditText sname = v.findViewById(R.id.name);
+                    final EditText sroll = v.findViewById(R.id.roll);
+                    final EditText snum = v.findViewById(R.id.number);
+                    Button update = v.findViewById(R.id.update);
+                    Button cancel = v.findViewById(R.id.cancel);
+
+                    final BottomSheetDialog dialog=new BottomSheetDialog(context);
+                    dialog.setContentView(v);
+                    dialog.setCancelable(false);
+
+                    sname.setText(name.getText().toString());
+                    sroll.setText(roll.getText().toString());
+                    sroll.setEnabled(false);
+                    snum.setText(number.getText().toString());
+
+                    update.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            RTable rTable = new RTable();
+                            rTable.setName(sname.getText().toString());
+                            rTable.setRoll(sroll.getText().toString());
+                            rTable.setNumber(snum.getText().toString());
+                            MainActivity.viewModel.update(rTable);
+                            Toast.makeText(context, "Data Updated",
+                                    Toast.LENGTH_SHORT).show();
+                            dialog.dismiss();
+                        }
+                    });
+                    cancel.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            dialog.dismiss();
+                        }
+                    });
+                    dialog.show();
+                }
+            });
         }
     }
 }
